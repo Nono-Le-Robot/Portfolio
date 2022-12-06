e8627094329cd0c85322b727eca919da34bdc8ac
nono <renaud.sannier@laposte.net>
2022-12-06 17:10:03 +0100

    100vh

--- back/app.js
+++ back/app.js
@@ -1,16 +1,15 @@
-require('./config/db')
-const bodyParser = require('body-parser');
-const express = require('express')
-const postRoutes = require('./routes/post.routes')
-const app = express()
+require("./config/db");
+const bodyParser = require("body-parser");
+const express = require("express");
+const postRoutes = require("./routes/post.routes");
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use('/portfolio/contact-mail/', postRoutes)
+app.use("/portfolio/contact", postRoutes);
 
 //server
 app.listen(5000, () => {
-    console.log(`listening on port 5000`);
+  console.log(`listening on port 5000`);
 });
--- js/contact.js
+++ js/contact.js
@@ -1,33 +1,28 @@
 const btn = document.querySelector("#envoyer");
 let sent = false;
 btn.addEventListener("click", () => {
+  let data = {
+    nom: nom.value,
+    prenom: prenom.value,
+    email: email.value,
+    message: texte.value,
+  };
+  nom.value = "";
+  prenom.value = "";
+  email.value = "";
+  texte.value = "";
+
   if (sent === false) {
     alert("Votre message a bien été envoyé.");
-
-    let data = {
-      nom: nom.value,
-      prenom: prenom.value,
-      email: email.value,
-      message: texte.value,
-    };
-    nom.value = "";
-    prenom.value = "";
-    email.value = "";
-    texte.value = "";
-    fetch("https://sannier-renaud.fr/portfolio/contact-mail/messages", {
+    fetch("https://sannier-renaud.fr/portfolio/contact/messages", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
-      .then((resp) => resp.json())
-      .then(() => {
+      .then((resp) => {
         sent = true;
+        resp.json();
       })
-      .catch((err) =>
-        alert(
-          "Il y a eu un probleme au moment de l'envoi, contactez moi par mail : sannier.renaud@gmail.com"
-        )
-      )
       .catch((err) =>
         alert(
           "Il y a eu un probleme au moment de l'envoi, contactez moi par mail : sannier.renaud@gmail.com"
